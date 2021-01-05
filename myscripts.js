1ece7d04184a4675adf40e67ef4a3ff2327d0e18
Debasri Dasgupta <debasri_dasgupta@rediffmail.com>
2021-01-05 21:45:50 +0100

    Separated background and text

--- myscripts.js
+++ myscripts.js
@@ -6,6 +6,7 @@ const left = document.querySelector('.left');
 const right = document.querySelector('.right');
 
 const slider = document.querySelector('.slider');
+const bgslider = document.querySelector('.backgroundSlider');
 const sections = document.querySelectorAll('.slider section');
 
 const indicatorParent = document.querySelector('.control ul');
@@ -18,6 +19,7 @@ indicators.forEach((indicator, i) => {
     document.querySelector('.control .selected').classList.remove('selected');
     indicator.classList.add('selected');
     slider.style.transform = 'translateX(' + (i) * -10 + '%)';
+    bgslider.style.transform = 'translateX(' + (i) * -5 + '%)';
     index = i;
 
   });
@@ -28,6 +30,7 @@ left.addEventListener('click', function () {
   index = (index > 0) ? index - 1 : 0;
   document.querySelector('.control .selected').classList.remove('selected');
   indicatorParent.children[index].classList.add('selected');
+  bgslider.style.transform = 'translateX(' + (index) * -5 + '%)';
   slider.style.transform = 'translateX(' + (index) * -10 + '%)';
   sections[index].style.webkitAnimationName = 'fadein';
   sections[index].style.webkitAnimationDuration = '5s';
@@ -38,6 +41,7 @@ right.addEventListener('click', function () {
   index = (index < 10 - 1) ? index + 1 : 9;
   document.querySelector('.control .selected').classList.remove('selected');
   indicatorParent.children[index].classList.add('selected');
+  bgslider.style.transform = 'translateX(' + (index) * -5 + '%)';
   slider.style.transform = 'translateX(' + (index) * -10 + '%)';
   sections[index].style.webkitAnimationName = 'fadein';
   sections[index].style.webkitAnimationDuration = '5s';
